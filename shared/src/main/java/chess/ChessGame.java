44a46c5aade81d51fecaeb13db0f1a3918df31ec
jinie <jiniejinie001125@gmail.com>
2025-02-04 16:21:23 -0700

    isInStalemate.

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -188,7 +188,25 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        throw new UnsupportedOperationException("Not Implemented Yet.");
+        if (isInCheck(teamColor)){
+            return false;
+        }
+
+        for (int row=1; row<=8;row++){
+            for (int col=1;col<=8;col++){
+                ChessPosition position = new ChessPosition(row, col);
+                ChessPiece piece = board.getPiece(position);
+
+                if(piece != null && piece.getTeamColor()==teamColor){
+                    Collection<ChessMove> validMoves = validMoves(position);
+
+                    if (validMoves != null && !validMoves.isEmpty()){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
 
     /**
